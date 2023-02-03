137c5bb5710db01a0600edf37ba3ae6fa56dc275
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-02-03 20:00:56 +0500

    create code_1.0

---
+++ HomeWork_1_seminar_7/Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[m,n];
+
+Random rnd = new Random();
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double a = genRndDouble(-10,10);
+        array[i,j] = a;
+        System.Console.Write($"{Math.Round(array[i, j],2)} ");
+    }
+    System.Console.WriteLine();
+}
+
+double genRndDouble (int a, int b)
+{
+   
+    double rndDouble = a + rnd.NextDouble() * (b-a);
+
+    return rndDouble;
+}
