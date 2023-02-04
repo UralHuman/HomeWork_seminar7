2a42ab7d4c037df70720169cba3a9fd88ab37bb8
Eduard Tebenkov_linux <9193633335w@gmail.com>
2023-02-04 19:51:59 +0500

    create code_2.0

---
+++ HomeWork_2_seminar_7/Program.cs
@@ -0,0 +1,31 @@
+﻿int[,] array = new int[4,5];
+
+Random rnd = new Random();
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array [i, j] = Convert.ToInt32(new Random().Next(10));  
+}
+ 
+for (int i = 0; i < array.GetLength(0); i++) 
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i,j] + "  ");
+        Console.WriteLine();
+}
+
+Console.Write("Введите индекс строки: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите индекс столбца: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>4 || b>5)
+
+Console.WriteLine("такого элемента нет");
+
+else
+{
+var c = array.GetValue(a,b);
+Console.WriteLine(c);
+}
